763c0b70b928f8b7e3b2144fd0a1d77b5cf0d7f0
Gabril D. Padua <gabriel.d.padua21@gmail.com>
2023-04-16 21:07:34 -0300

    feat: ✨ create category service

---
+++ internals/service/category.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"context"
+
+	"github.com/gabrielDpadua21/fc-grpc-essentialst/internals/database"
+	"github.com/gabrielDpadua21/fc-grpc-essentialst/internals/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{CategoryDB: categoryDB}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return &pb.CategoryResponse{
+		Category: categoryResponse,
+	}, nil
+}
