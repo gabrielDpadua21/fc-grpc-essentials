f894ae7edffd7b6c7a111deb70944ccf028782f9
Gabril D. Padua <gabriel.d.padua21@gmail.com>
2023-05-23 01:27:05 -0300

    fix: 🐞 fix create category service

--- internals/service/category.go
+++ internals/service/category.go
@@ -16,19 +16,21 @@ func NewCategoryService(categoryDB database.Category) *CategoryService {
 	return &CategoryService{CategoryDB: categoryDB}
 }
 
-func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
-	category, err := c.CategoryDB.Create(in.Name, in.Description)
+func (c *CategoryService) CreateCategory(ctx context.Context, input *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(input.Name, input.Description)
 	if err != nil {
 		return nil, err
 	}
 
-	categoryResponse := &pb.Category{
+	newCategory := &pb.Category{
 		Id:          category.ID,
 		Name:        category.Name,
 		Description: category.Description,
 	}
 
-	return &pb.CategoryResponse{
-		Category: categoryResponse,
-	}, nil
+	categoryResponse := &pb.CategoryResponse{
+		Category: newCategory,
+	}
+
+	return categoryResponse, nil
 }
